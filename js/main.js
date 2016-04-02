7e37c93c779ee54628d1cfb1f4aa8fdcb905ce5a
Johnyvan333 <vanchez666@gmail.com>
2016-04-02 18:54:26 +0300

    закончил с графикой

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var menuOpen = document.querySelector (".top-panel__toogle");
 var menuPopup = document.querySelector(".main-menu");
 var menuClose = menuPopup.querySelector(".main-menu__close-icon");
 
+menuPopup.classList.remove("main-menu--open");
+
 menuOpen.addEventListener("click", function(event) {
   event.preventDefault();
   menuPopup.classList.add("main-menu--open");
